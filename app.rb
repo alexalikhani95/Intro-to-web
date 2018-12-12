6c06f0f01c626c2bccd04fbc1a0fddde720925b3
Alex <alikhanialex@gmail.com>
2018-12-12 14:48:58 +0000

    introducing params

--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
 require 'sinatra'
 
-get '/' do
-  'hello!'
+get '/random-cat' do
+  @name = ["Dave", "Fred", "John"].sample
+  erb(:index)
 end
 
-get '/secret' do
-  'This is a secret page'
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
 end
